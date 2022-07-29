75a9d84510082c6690383b87e354e5dcb43cc176
shivanshinde <104614023+shivanshinde@users.noreply.github.com>
2022-07-29 23:27:46 +0530

    Add files via upload

---
+++ 14.Assignment/05.Accept Number From User And Display Below Pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Pattern(int);
+
+int main()
+{
+    int No = 0;
+
+    printf("\n\t Enter Number Of Element => ");
+    scanf("%d",&No);
+
+    Pattern(No);
+
+    return 0;
+}
+void Pattern(int Num)
+{
+    int i = 0;
+
+    for(i = 1;i <= Num;i++)
+    {
+        printf("\t%d",i*2);
+    }
+    getch();
+    return 0;
+}
